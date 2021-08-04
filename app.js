8ea0cfd5a4c6efb3581eeb539af5a028d5d8213c
OzerOzturk <ozerozturk1636@gmail.com>
2021-08-05 00:27:38 +0300

    layout for the home section okay with responsive design

---
+++ app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+
+    /* toogle menu/navbar script  */
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+
+
+
+
+});
